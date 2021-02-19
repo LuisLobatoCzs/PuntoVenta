21f45f1c9f3472b457a7f57e6641af2206783263
Marianagomezislas <mariana.gomez@fstack.com.mx>
2021-02-18 23:33:24 -0600

    employees y products responsivo

--- resources/views/employees.blade.php
+++ resources/views/employees.blade.php
@@ -1,47 +1,61 @@
+<style>
+    .c1{
+        background: #5Cf600;
+    }
+    .c2{
+        background: #4bc200;
+    }
+    .centrarY {
+        align-items: center;
+    }
+</style>
 @extends('layouts.app')
 
 @section('content')
-<br>
-<br>
-<div class="row">
-    
-        <div class="col-9 -sm-6 text-center">
-            <h3>Administración de empleados</h3>
-        </div>
-        <div class="col-3 sm-1">
-            <button class="btn btn-warning">Agregar Empleado</button>
-        </div>   
-    <br><br><br><br>
- </div>    
-<div class="row">
-    <div class="col-1"></div>
-        <div class="col-10 sm-6" align="center">
-            <table class="table">
-                <thead class="thead">
-                    <tr>
-                        <th>Nombre</th>
-                        <th>Email</th>
-                        <th>Telefono</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>Mariana G</td>
-                        <td>2214068655</td>
-                        <td>marigoislas@gmail.com</td>
-                        <td><button class="btn btn-info">Modificar</button></td>
-                        <td><button class="btn btn-secondary ">Eliminar</button></td>
-                    </tr>
-                    <tr>
-                        <td>Luis L</td>
-                        <td>2214068655</td>
-                        <td>lalc@gmail.com</td>
-                        <td><button class="btn btn-primary">Modificar</button></td>
-                        <td><button class="btn btn-secondary">Eliminar</button></td>
-                    </tr>
-                </tbody>
-            </table>
-        </div> 
-    <div class="col-1"></div>  
-</div>
+<div class="col-12">
+    <br><br>
+    <div class="row centraY">
+        
+            <div class="col-12 col-md-8 text-center">
+                <h4>Administrar empleados</h4>
+            </div>
+            <div class="col-12 col-md-3 text-center">
+                <button class="btn btn-warning col-10">Agregar Empleado</button>
+            </div>   
+        
+    </div>    
+    <div class="row">
+        <div class="col-1"></div>
+            <div class="col-12 col-md-11  align=center">
+                <div class="table-responsive">
+                    <table class="table">
+                        <thead class="thead">
+                            <tr>
+                                <th>Nombre</th>
+                                <th>Email</th>
+                                <th>Telefono</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Mariana G</td>
+                                <td>2214068655</td>
+                                <td>marigoislas@gmail.com</td>
+                                <td><button class="btn btn-info">Modificar</button></td>
+                                <td><button class="btn btn-secondary ">Eliminar</button></td>
+                            </tr>
+                            <tr>
+                                <td>Luis L</td>
+                                <td>2214068655</td>
+                                <td>lalc@gmail.com</td>
+                                <td><button class="btn btn-info">Modificar</button></td>
+                                <td><button class="btn btn-secondary">Eliminar</button></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div> 
+        <div class="col-1"></div>  
+    </div>
+</div> 
 @endsection
